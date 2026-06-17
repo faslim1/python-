37ea088f395915cab7e05f40f58578635d9ce138
faslim1 <faslimrahim123@gmail.com>
2026-06-17 21:11:53 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,116 @@
+
+#last_name = ' rahim'
+#print(first_name  +  last_name + "\tgood")
+
+
+# python is case sensitive
+# use underscore for variable which is easy to understand
+# use names likes(eg: age) and not a or b
+ 
+A = 15 
+a = 5
+#print(id(A))
+
+#swapping 2 numbers
+a = 2
+b = 5
+c = a
+a = b
+b = c
+
+#print(a)
+#print(b)
+
+# a = 7 
+# b = 1 
+# a = a + b
+# b = a - b
+# a = a - b
+#print(a)
+#print(b)
+
+#data types
+a = -5.3 #sign is not considered
+#print(type(a))
+
+a = 2 
+#print(type(a))
+
+#operators
+ 
+#addition
+
+a = 2 
+b = 4
+#print(a+b)
+ 
+#subtraction -
+
+#multiplication *
+
+#division /
+#print(a/b)
+
+#comparison operators
+
+
+#equal to 
+a = 3
+b = 6
+# print(a == b)
+# not equal to
+
+print(a != b )
+# less than operator
+# print(a < b)
+
+#greater than or equal to
+#print(a >= b)
+
+#less than or equal to
+
+#print(a <= b)
+
+  #conditional statement
+
+age = 18 
+# if
+if age>=18 :
+    print("eligible")
+else :
+    print("not eligible")
+print('\n')
+#check if a number is odd or even
+a = 6
+if a%2==0:
+    print('even')
+else:
+    print('odd')
+print('\n')
+#if else
+
+
+
+
+
+mark = 64
+
+if mark>90:
+    print('a+')
+elif mark>80:
+    print('a')
+elif mark>70:
+    print('b+')
+elif mark>60:
+    print('b')
+
+
+# logical operators
+
+a = 7 
+b = 10
+if a>15 and b==10:
+    print('yes')
+
+
+
