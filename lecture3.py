6cba7cc66782162713c36b2c9d03ea341e38ba0d
faslim1 <faslimrahim123@gmail.com>
2026-06-24 11:01:31 +0530

    Add files via upload

---
+++ lecture3.py
@@ -0,0 +1,65 @@
+# in operator or membership operator
+
+# in operator or membership operator
+
+# a = (input('Enter the string: '))
+# v = ['a','e','i','o','u','A','E','I','O','U']
+# for i in range(len(a)):
+#     if a[i] in v :
+#         print(i,a[i],',',end=' ')
+print('/i')
+# create a list of even numbers and odd numbers of first 100 numbers
+
+e = []
+o = []
+a = 101
+for i in range(101):
+    if i%2==0:
+        e.append(i)
+        
+    else:
+        o.append(i)
+print(f'Even number = {e}')
+print(f'Odd number = {o}')
+
+#remove duplicates from the list
+
+# l = [1,2,3,4,1,3,4,2,7,8,9,2,3,4,5]
+# b = []
+# for i in l:
+#     if i not in b:
+#         b.append(i)
+# print(b)
+
+
+# pass,continue,break statement
+
+age = [3,4,5,6]
+if age==3:
+    pass
+
+while age==5:
+    break
+
+while age==4:
+    continue
+
+print(age)
+
+
+# input n words and make it into a sentence
+# reverse of a string without usning [::-1]
+
+
+
+
+
+        
+
+        
+
+
+
+
+
+
