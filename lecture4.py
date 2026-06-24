74a296ec81179d09fc137a1eb5ed89d6364bc379
faslim1 <faslimrahim123@gmail.com>
2026-06-24 11:01:53 +0530

    Add files via upload

---
+++ lecture4.py
@@ -0,0 +1,76 @@
+# set --{}
+# set is unordered that is no index position
+# contains no duplicate values
+# mutable
+
+# a = {1,2,3,5,6,8,1,4,2,5,6,7}
+# print(a)
+
+# a = {1,2,3}
+# b = {3,1,2}
+# print(a==b)
+
+# fruits = {'watermelon','banana','orange','guava'}
+# for i in fruits:
+#     print(i)
+
+# a = {1,2,3,4,5,6}
+# b = {1,2,3,7,8,9}
+# print(a|b)
+# print(a.union(b))
+# print(a.intersection(b))
+# print(a&b)
+# print(a-b)
+# print(b-a)
+
+# dictionary
+# dictionary are mutable
+# dictionary is dynamic
+
+
+# data = {}
+# data['name'] = "das"
+# data['age'] = 29
+# data['email'] = 'daas@123'
+# data['phone'] = 989765090
+# data['name'] = 'faslim'
+# data['email'] = 'faslim@gmail.com'
+# data['age'] = 21
+
+
+# data['name'] = input('Enter Name: ')
+# data['age'] = input('Enter Age: ')
+print('/a')
+
+# print(data)
+
+# a = {'name': 'asif','age':22,'location':'kochi'}
+# print(a.get('name'))
+# print(a['location'])
+# print(a.keys())
+# print(a.values())
+# print(a.items())
+# print(a.update({'name':'ashiq'}))
+# a.popitem()
+# a.clear()
+# print(a)
+
+# data = {'name':''}
+# for i,j in a.items():
+#     print(i,j)
+
+b = 'hello my name is faslim'
+word = ''
+c = []
+for i in b:
+    if i !=' ':
+        word=word+i
+    else:
+        c.append(word)
+        word=''
+c.append(word)
+print(c)
+
+# leetcode lemonade
+
+
