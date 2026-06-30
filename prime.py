77e3ba193bef29694dcbf67a0f4a02f4da989d25
faslim1 <faslimrahim123@gmail.com>
2026-06-30 23:25:31 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,15 @@
+# check if number is prime or not    
+
+a = int(input('Enter the number: '))
+if a>1:
+    for i in range (2,a):
+        if a%i==0:
+            print(f'{a} is not a prime number')
+            break
+    else:
+        print(f'{a} is a prime number')
+else:
+    print(f'{a} is not a prime number')
+
+
+
