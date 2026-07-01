391bf60a69ddc636455720311402af3a1dbaf228
faslim1 <faslimrahim123@gmail.com>
2026-07-02 00:05:47 +0530

    Add files via upload

---
+++ lecture7.py
@@ -0,0 +1,63 @@
+# hollow square pattern
+for i in range(1,7):
+    if i==1 or i==6:
+        print('******** ')
+    else:
+        print('*      *')
+
+# i=row
+# j==colomn
+
+# for i in range(1,6):
+#     for j in range(1,4):
+#         if i==1 or i==3 or i==5 or j==2 :
+#             print('* ',end='')
+#         elif 
+#             print('',end='')
+#         if j==2 and j==:
+#             print()
+
+
+
+def add2(a,b):
+    return 5+4
+    
+# using lambda function
+
+
+z = lambda a,b : a+b   
+print(z(2,35))
+
+
+
+
+ 
+
+# square root of a number   
+# full name of a person
+
+# product of 3 numbers
+z = lambda a,b,c : a*b*c 
+print(z(3,5,6))
+
+# square of a numbers
+z = lambda a : a**2
+print(z(6))
+
+# perimeter of a circle
+z = lambda r : 2*3.14*r
+print(z(15))
+
+# area of a triangle
+z = lambda a,b : 0.5*a*b
+print(z(5,8))
+
+# average of 5 numbers
+z = lambda a,b,c,d,e : (a*b*c*d*e/5)
+print(z(3,4,5,6,7))
+
+
+# ternary operator
+
+z = lambda fullname('fname'+' '+'mname'+' '+'lname')
+
