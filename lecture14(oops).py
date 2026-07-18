617c43c23e085e2aad573628edb562f31b0d239a
faslim1 <faslimrahim123@gmail.com>
2026-07-18 21:12:10 +0530

    Add files via upload

---
+++ lecture14(oops).py
@@ -0,0 +1,4 @@
+# write a python class queue thst implements a basic queue data structure with the enqueue and dequeu methods
+# the enqueue method should add an elemnt to the rear of the queue and the deque structure should remove an elemnt form the queue
+# additionally include a methof is_empty to check if the queu is empty
+
